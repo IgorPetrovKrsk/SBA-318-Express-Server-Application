a4175ae334851da264b1d41e2776ba5849160707
IgorPetrovKrsk <igor.petrov.krsk@gmail.com>
2025-04-17 11:43:24 -0600

    setting up typescript in node

--- error/error.mjs
+++
@@ -1,7 +0,0 @@
-function error(status, msg) {
-  var err = new Error(msg);
-  err.status = status;
-  return err;
-}
-
-export default error;
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import error from './error/error.mjs';
+import express, { Request, Response, NextFunction } from 'express';
+import error from './error/error';
 
 //setup
 const app = express();
-const PORT = 3000 || 3001;
+const PORT = 3000;
 
 
 //middleware
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 });
 
 //error handling
-app.use((err, req, res, next) => {
+app.use((err: any, req:Request, res:Response, next: NextFunction) => {
     res.status(err.status || 500);
     res.json({ error: err.message });
   });
